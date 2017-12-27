d9a645ef79479c10209022be626f02f249f4611c
fedyhendrawan3 <33412826+fedyhendrawan3@users.noreply.github.com>
2017-12-27 15:07:19 +0700

    Update Bug
    Master Data Jenis Bahan
    Master Data Warnaglasin

--- application/APP_CI/modules/Mjbahan/controllers/Mjbahan.php
+++ application/APP_CI/modules/Mjbahan/controllers/Mjbahan.php
@@ -2,41 +2,43 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mjbahan extends MX_Controller {
-	
-	public function index(){
-            $this->load->view('V_mjbahan');
-	}
+
+    public function index(){
+       $this->load->view('V_mjbahan');
+    }
+    
+    public function read(){
+        $this->load->model('R_mjbahan');
+        $start = $this->input->post('start');
+        $limit = $this->input->post('limit');
+        $filter = $this->input->post('filter');
+        header('Content-type: application/json');
+        print_r( $this->R_mjbahan->load_default($start,$limit,$filter));
         
-        public function read(){
-            $this->load->model('R_mjbahan');
-            $start = $this->input->post('start');
-            $limit = $this->input->post('limit');
-            $filter = $this->input->post('filter');
-            header('Content-type: application/json');
-            print_r( $this->R_mjbahan->load_default($start,$limit,$filter));
+    }
 
-        }        
-
-        public function create(){        
-            $jsonData =  file_get_contents("php://input");
-            $this->load->model('C_mjbahan');
-            $this->C_mjbahan->insertDT(json_decode($jsonData,true));
-
-        }
-        public function update(){
-            $jsonData =  file_get_contents("php://input");        
-            $this->load->model('U_mjbahan');
-            $this->U_mjbahan->updateDT(json_decode($jsonData,true));
-        }
-        public function delete(){
-            $jsonData =  file_get_contents("php://input");        
-            $this->load->model('D_mjbahan');
-            $this->D_mjbahan->deleteDT(json_decode($jsonData,true));
-        }
-        public function cbolist(){
+    public function create(){   
+        $jsonData =  file_get_contents("php://input");
+        $this->load->model('C_mjbahan');
+        $hasil = $this->C_mjbahan->insertDT(json_decode($jsonData,true));
+        print($hasil); 
+    }
+    public function update(){
+        $jsonData =  file_get_contents("php://input");        
+        $this->load->model('U_mjbahan');
+        $hasil = $this->U_mjbahan->updateDT(json_decode($jsonData,true));
+        print($hasil);
+    }
+    public function delete(){
+        $jsonData =  file_get_contents("php://input");        
+        $this->load->model('D_mjbahan');
+        $hasil = $this->D_mjbahan->deleteDT(json_decode($jsonData,true));
+        print($hasil);
+    }
+    public function cbolist(){
         $this->load->model('R_mjbahan');
         header('Content-type: application/json');
         print_r($this->R_mjbahan->cbolist());
     }
-    
-};
+            
+}
